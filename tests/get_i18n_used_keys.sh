35f770cc560ad1871b586a16074925963d43e9ba
Rafael Mori <faelmori@gmail.com>
2025-09-16 21:47:32 -0300

    fix: atualizar caminho do diretório de origem para ./frontend/src no script de análise de chaves i18n
    Signed-off-by: Rafael Mori <faelmori@gmail.com>

--- tests/get_i18n_used_keys.sh
+++ tests/get_i18n_used_keys.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ajuste ./src se precisar
-rg -no --pcre2 "t\(\s*['\"`]([A-Za-z][\w-]+)\.([A-Za-z0-9_.-]+)['\"`]\s*(?:,|\))" ./src \
+rg -no --pcre2 "t\(\s*['\"\$(]([A-Za-z][\w-]+)\.([A-Za-z0-9_.-]+)['\")]\s*(?:,|\))" ./frontend/src \
 | awk -F: '{print $3}' \
 | sed -E "s/^t\(['\"\`]//; s/['\"\`].*$//" \
 | sort -u > i18n_used_keys.txt
